03daa9516cb8da5de5307660ecb072bd1212ec3d
Ben Carey <bncarey42@gmail.com>
2019-04-01 15:32:50 -0500

    Update Actor.js

--- battle-tracker/js/Actor.js
+++ battle-tracker/js/Actor.js
@@ -33,7 +33,6 @@ class Actor {
     initiative.setAttribute('type','number');
     initiative.setAttribute('name', this.index + '_initiative');
     initiative.setAttribute('value', this.initiative);
-    initiative.setAttribute('onchange', 'updateActorBlocks()');
 
     var name = document.createElement('input');
     name.setAttribute('type','text');
