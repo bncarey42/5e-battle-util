1089567d65b7785886d0fa14aa61abf84d04cf1d
Ben Carey <ITC62@securian.com>
2019-06-06 13:22:02 -0500

    updated wild magic util for new rule

--- wild-magic/js/wildmagic.js
+++ wild-magic/js/wildmagic.js
@@ -1,10 +1,38 @@
 function dieOneHundred() {
-  return Math.floor(Math.random() * 100) + 1)
+  return Math.floor(Math.random() * (100 + 1))
 }
 
-function wildMagic(var roll, var lvl) {
-  if(lvl < 6) { return 0.2 >= roll/100 }
-  else if(lvl < 11) { return 0.15 >= roll/100 }
-  else if(lvl < 16) { return 0.1 >= roll/100 }
-  else if(lvl < 20) { return 0.05 >= roll/100 }
+function wildMagic(roll, lvl) {
+  if(lvl < 6)       { return 20 >= roll }
+  else if(lvl < 11) { return 15 >= roll }
+  else if(lvl < 16) { return 10 >= roll }
+  else if(lvl < 20) { return 5  >= roll }
+}
+
+function surge(roll) {
+  return surgeTable[roll]
+}
+
+function cast() {
+  var result = document.getElementById('surge_result')
+  result.innerHTML =""
+  result.setAttribute('style', randomColor());
+  var lvl = document.getElementById('lvl').value
+  var roll = dieOneHundred()
+  console.log(roll +", "+lvl);
+
+  if(wildMagic(Number(roll), lvl)) {
+    var surge_text = document.createTextNode(surge(Number(dieOneHundred())))
+    console.log("IT HAPPENS : " + surge_text);
+    result.appendChild(surge_text)
+  } else {
+    var surge_text = document.createTextNode("Nothing happens")
+    result.appendChild(surge_text)
+  }
+}
+
+function randomColor() {
+  var colors = [ '--cyan', '--blue', '--purple', '--green', '--red', '--orange', '--light-orange']
+
+  return 'color:var(' + colors[Math.floor(Math.random() * (colors.length))] + ');'
 }
