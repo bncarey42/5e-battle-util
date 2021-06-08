f3176bfa3e37eb4ed0f138920f1769ec604868b2
ben <ben@MacBook-Pro-88.local>
2021-06-08 12:24:10 -0500

    this is a start need better end of name terminator

--- battle-tracker/js/vue.js
+++ battle-tracker/js/vue.js
@@ -1,23 +1,23 @@
 Vue.component('actor_row', {
     props: ['pc'],
-    template: '<div class="row">' +
-                '<div class="three columns">' +
-                  '<input type="number" v-model="pc.init">'+
-                '</div>' +
-                '<div class="four columns">' +
-                  '<input type="text" v-model="pc.name">' +
-                '</div>'+
-                '<div class="one columns">' +
-                  '<input type="number" v-model="pc.hp">' +
-                '</div>' + 
-                '<div class="one columns">' +
-                  '<input type="number" v-model="pc.hp_change">' +
-                '</div>' + 
-                '<div class="three columns">' +
-                  '<button class="damage_btn" style="background-color:var(--red)" @click=damage>damage</button>' +
-                  '<button class="heal_btn" style="background-color:var(--green)" @click=heal>heal</button>' +
-                '</div>' + 
-              '</div>',
+    template: `<div class="row">
+                <div class="three columns">
+                  <input type="number" v-model="pc.init" style="width:100%;">
+                </div>
+                <div class="three columns">
+                  <input type="text" v-model="pc.name" style="width:100%;">
+                </div>
+                <div class="one columns">
+                  <input type="number" v-model="pc.hp" style="width:100%;">
+                </div> 
+                <div class="one columns">
+                  <input type="number" v-model="pc.hp_change" style="width:100%;">
+                </div>
+                <div class="four columns">
+                  <button class="damage_btn" style="background-color:var(--red)" @click=damage>damage</button>
+                  <button class="heal_btn" style="background-color:var(--green)" @click=heal>heal</button>
+                </div>
+              </div>`,
     methods: {
       damage: function() {this.pc.hp = Number(this.pc.hp) - Number(this.pc.hp_change)},
       heal: function() {this.pc.hp = Number(this.pc.hp) + Number(this.pc.hp_change)}
